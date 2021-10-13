1a91ccb361f8b7c2a54f16e09b1ea0e0b0014653
Robert Chambers <1robertchambers@gmail.com>
2021-10-13 22:42:22 +0100

    test: function to get smallest unvisited vertex returns correct vertex

---
+++ src/tests/test_get_smallest_in_unvisited.c
@@ -0,0 +1,53 @@
+#include "dijkstra.h"
+#include <stdio.h>
+
+void test_smallest() {
+	int unvisited[] = {1, 0, 0, 0};
+	int shortest_distances[] = {0, 4, 3, 5};
+	int n_vertexes = 4;
+
+	int smallest_result = get_smallest_in_unvisited(unvisited, shortest_distances, n_vertexes);
+
+	if (smallest_result == 2) 
+		printf("test_smallest passed\n");
+	else
+		printf("test_smallest failed\n");
+}
+
+void test_skips_visited() {
+	int unvisited[] = {1, 0, 0, 0, 1, 1};
+	int shortest_distances[] = {0, 4, 3, 5, 2, 1};
+	int n_vertexes = 6;
+
+	int smallest_result = get_smallest_in_unvisited(unvisited, shortest_distances, n_vertexes);
+
+	if (smallest_result == 2) 
+		printf("test_skips_visited passed\n");
+	else
+		printf("test_skips_visited failed\n");
+}
+
+void test_skips_not_connected() {
+	int unvisited[] = {1, 0, 0, 0};
+	int shortest_distances[] = {0, -1, -1, 5};
+	int n_vertexes = 4;
+
+	int smallest_result = get_smallest_in_unvisited(unvisited, shortest_distances, n_vertexes);
+
+	if (smallest_result == 3) 
+		printf("test_skips_not_connected passed\n");
+	else
+		printf("test_skips_not_connected failed\n");
+}
+
+int main(int argc, char **argv) {
+	printf("---In test_get_smallest_in_unvisited---\n");
+	test_smallest();
+
+	test_skips_visited();
+
+	test_skips_not_connected();
+
+	printf("------\n");
+	return 0;
+}
