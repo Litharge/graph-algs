d623e65d5cd07b8b0b408a88eb3fb41bfa18d784
Robert Chambers <1robertchambers@gmail.com>
2021-10-16 11:42:06 +0100

    feat: main dijkstra function to run the algorithm

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -9,7 +9,7 @@ int get_smallest_in_unvisited(int *unvisited, int *shortest_distances, int n_ver
 	for (int i = 0; i < n_vertexes; i++) {
 		if (unvisited[i] == 0 && (shortest_distances[i] <= min_dist || min_dist == -1) && shortest_distances[i] != -1) {
 			min_dist = shortest_distances[i];
-		       min_dist_vertex = i;
+			min_dist_vertex = i;
 		}
 	}
 
@@ -40,3 +40,35 @@ void print_arr(int *to_print, int n) {
 	printf("\n");
 }
 
+
+int *dijkstra_alg(int **graph, int n_vertexes, int target) {
+	// shortest_distances holding shortest distance to each vertex
+	int *shortest_distances = malloc(n_vertexes * sizeof(int));
+
+	// designate all vertexes not visited
+	for (int i = 0; i < n_vertexes; i++)
+		shortest_distances[i] = -1;
+
+
+	// unvisited[i] holding 0 for vertex i unvisited, 1 visited
+	int *unvisited = calloc(n_vertexes, sizeof(int));
+
+	unvisited[0] = 1;
+	shortest_distances[0] = 0;
+
+	while(1) {
+		int current = get_smallest_in_unvisited(unvisited, shortest_distances, n_vertexes);
+		printf("current: %d\n", current);
+		print_arr(unvisited, n_vertexes);
+		print_arr(shortest_distances, n_vertexes);
+
+		add_connections_to_shortest(current, shortest_distances, unvisited, graph, n_vertexes);
+
+		unvisited[current] = 1;
+
+		if (unvisited[target] == 1) {
+			free(unvisited);
+			return shortest_distances;
+		}
+	}
+}
--- src/dijkstra.h
+++ src/dijkstra.h
@@ -1,7 +1,7 @@
 #ifndef DIJKSTRA_H_
 #define DIJKSTRA_H_
 
-void dijkstra_alg();
+int * dijkstra_alg(int **graph, int n_vertexes, int target);
 
 int get_smallest_in_unvisited(int *unvisited, int *shortest_distances, int n_vertexes);
 
