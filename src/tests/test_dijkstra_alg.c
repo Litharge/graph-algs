c059c300b0a7b228adc3dff205f7675d11d2d3c9
Robert Chambers <1robertchambers@gmail.com>
2021-10-16 14:28:39 +0100

    test: dijkstra alg works with more complex graph

--- src/tests/test_dijkstra_alg.c
+++ src/tests/test_dijkstra_alg.c
@@ -30,9 +30,44 @@ void test_dijkstra_simple_graph() {
 
 }
 
+void print_arr_1(int *to_print, int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d,", to_print[i]);
+	}
+	printf("\n");
+}
+
+void test_dijkstra_longer_graph() {
+	int **test_graph = malloc(6 * sizeof(int*));
+
+	int test_graph_row_0[] = {-1, 1, 2, -1, -1, -1};
+	int test_graph_row_1[] = {1, -1, -1, 6, 1, -1};
+	int test_graph_row_2[] = {2, -1, -1, 4, -1, -1};
+	int test_graph_row_3[] = {-1, 6, 4, -1, -1, 1};
+	int test_graph_row_4[] = {-1, 1, -1, -1, -1, 10};
+	int test_graph_row_5[] = {-1, -1, -1, 1, 10, -1};
+
+	test_graph[0] = test_graph_row_0;
+	test_graph[1] = test_graph_row_1;
+	test_graph[2] = test_graph_row_2;
+	test_graph[3] = test_graph_row_3;
+	test_graph[4] = test_graph_row_4;
+	test_graph[5] = test_graph_row_5;
+
+	int *dists = dijkstra_alg(test_graph, 6, 5);
+
+	print_arr_1(dists, 6);
+
+	if (dists[5] == 7) {
+		printf("test_dijkstra_longer_graph passed\n");
+	}
+}
+
+
 int main(int argc, char **argv) {
 	printf("---In test_dijkstra_alg---\n");
 	test_dijkstra_simple_graph();
+	test_dijkstra_longer_graph();
 
 	return 0;
 }
