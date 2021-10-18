54cb344dfcce169eeca1f89a144f6823be9f2f60
Robert Chambers <1robertchambers@gmail.com>
2021-10-18 13:03:30 +0100

    docs: comments for interface function and struct

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -1,6 +1,14 @@
 #ifndef DIJKSTRA_H_
 #define DIJKSTRA_H_
 
+
+/* Struct: PathInfo
+ * struct describing a shortest path
+ *
+ * shortest_distances: tentative shortest distances to each vertex
+ * path: the vertexes in the shortest path
+ * path_length: the length of the shortest path
+ */
 typedef struct PathInfo {
 	int *shortest_distances;
 	int *path;
@@ -8,7 +16,15 @@ typedef struct PathInfo {
 } PathInfo;
 
 
-
+/* Function: dijkstra_alg
+ * runs dijkstra's algorithm on a graph matrix, funding the shortest path from vertex 0 to a target vertex
+ *
+ * graph: matrix describing graph, with -1 denoting no connection
+ * n_vertexes: number of vertexes in the graph
+ * target: integer specifying the target vertex
+ *
+ * returns: PathInfo describing the path and giving its total length, if the path_length member variable is equal to -1, then the other member variables contents are undefiend
+ */
 PathInfo dijkstra_alg(int **graph, int n_vertexes, int target);
 
 int get_smallest_in_unvisited(int *unvisited, int *shortest_distances, int n_vertexes);
